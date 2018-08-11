ec1f557cd52dff62023d3ae27fb36ac76897b791
Joern Bernhardt <joern.bernhardt@googlemail.com>
2018-08-11 12:52:33 +0200

    Add a live editor for a shader
    Signed-off-by: Joern Bernhardt <joern.bernhardt@googlemail.com>

--- main.js
+++ main.js
@@ -1,56 +1,88 @@
+var currentShader;
 var prelImg;
 
-var CHANNEL = {
-    RED: 0,
-    GREEN: 1,
-    BLUE: 2,
-    ALPHA: 3
-}
+var imageUrl = "https://images.pexels.com/photos/981062/pexels-photo-981062.jpeg?auto=compress&cs=tinysrgb&h=400&w=710";
 
-function preload() {
-    prelImg = loadImage("https://images.pexels.com/photos/981062/pexels-photo-981062.jpeg?auto=compress&cs=tinysrgb&h=400&w=710");
+window.CHANNEL = {
+  RED: 0,
+  GREEN: 1,
+  BLUE: 2,
+  ALPHA: 3
+};
+
+const $updateButton = document.getElementById("update");
+$updateButton.addEventListener("click", function(ev) {
+  ev.preventDefault();
+  updateShader();
+});
+
+function updateShader() {
+  const imageUrl = document.getElementById("image").value;
+  const $textarea = document.getElementById("shader");
+  const shaderCode = $textarea.textContent;
+  const theShader = `function newShader(currentPixel, index, pixels, imgWidth, imgHeight) {
+		const newPixel = [0, 0, 0, 255];
+		
+		${shaderCode};
+		
+		return newPixel;
+	};
+	
+	currentShader = newShader;
+	`;
+  var oldScript = document.getElementById("scriptContainer");
+  var newScript;
+
+  if (oldScript) {
+    oldScript.parentNode.removeChild(oldScript);
+  }
+
+  newScript = document.createElement("script");
+  newScript.id = "scriptContainer";
+  newScript.text = theShader;
+  document.body.appendChild(newScript);
+
+  loadImage(imageUrl, function(img) {
+    createCanvas(img.width, img.height);
+    image(transform(img), 0, 0, img.width, img.height);
+		img.loadPixels();
+	});
 }
 
 function setup() {
-    createCanvas(prelImg.width, prelImg.height);
-
-    loadImage('https://images.pexels.com/photos/981062/pexels-photo-981062.jpeg?auto=compress&cs=tinysrgb&h=400&w=710', function (img) {
-        image(transform(img), 0, 0, prelImg.width, prelImg.height);
-    });
-
-    prelImg.loadPixels();
+  updateShader();
 }
 
 function transform(img) {
-    img.loadPixels();
-    applyPixelsArrayTo(toPixelArray(img.pixels), img);
-    img.updatePixels();
-    return img;
+  img.loadPixels();
+  applyPixelsArrayTo(toPixelArray(img.pixels), img);
+  img.updatePixels();
+  return img;
 }
 
 function toPixelArray(uInt) {
-    var pixelsArray = [];
-    for (var i = 0; i <= uInt.length; i += 4) {
-        pixelsArray[i / 4] = [
-            uInt[i + CHANNEL.RED],
-            uInt[i + CHANNEL.GREEN],
-            uInt[i + CHANNEL.BLUE],
-            uInt[i + CHANNEL.ALPHA]
-        ];
-    }
-    return pixelsArray;
+  var pixelsArray = [];
+  for (var i = 0; i <= uInt.length; i += 4) {
+    pixelsArray[i / 4] = [
+      uInt[i + CHANNEL.RED],
+      uInt[i + CHANNEL.GREEN],
+      uInt[i + CHANNEL.BLUE],
+      uInt[i + CHANNEL.ALPHA]
+    ];
+  }
+  return pixelsArray;
 }
 
 function applyPixelsArrayTo(pixelsArray, img) {
-    pixelsArray.forEach((pixel, i) => {
-      var newPixel = pixelShader(pixel, i , pixelsArray)
-        img.pixels[i * 4 + CHANNEL.RED] = newPixel[CHANNEL.RED];
-        img.pixels[i * 4 + CHANNEL.GREEN] = newPixel[CHANNEL.GREEN];
-        img.pixels[i * 4 + CHANNEL.BLUE] = newPixel[CHANNEL.BLUE];
-        img.pixels[i * 4 + CHANNEL.ALPHA] = newPixel[CHANNEL.ALPHA];
-    });
+  pixelsArray.forEach((pixel, i) => {
+    var newPixel = pixelShader(img, pixel, i, pixelsArray);
+    img.pixels[i * 4 + CHANNEL.RED] = newPixel[CHANNEL.RED];
+    img.pixels[i * 4 + CHANNEL.GREEN] = newPixel[CHANNEL.GREEN];
+    img.pixels[i * 4 + CHANNEL.BLUE] = newPixel[CHANNEL.BLUE];
+    img.pixels[i * 4 + CHANNEL.ALPHA] = newPixel[CHANNEL.ALPHA];
+  });
 }
 
-function pixelShader(pixel, index, pixels) {
-  return currentShader(pixel, index, pixels, prelImg.width, prelImg.height);
+function pixelShader(img, pixel, index, pixels) {
+  return currentShader(pixel, index, pixels, img.width, img.height);
 }
